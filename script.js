6c53a78f87487b4c116bc08e31fba5fa8602a50d
MinhAn1808 <ankhangloc12345@gmail.com>
2024-10-22 00:22:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+function displayCSD() {
+    let screen = document.getElementById("screen-img");
+    screen.style.backgroundImage = "url(../img/CSD.jpg)";
+    screen.style.display = "block"
+
+}
+function displayCSDInfo() {
+    let screen = document.getElementById("screen-info");
+    screen.innerHTML = "This is a document about data structures and algorithms"
+    screen.style.background = "#fac1fa"
+    screen.style.fontSize = "50px"
+    screen.style.display = "block"
+    screen.style.textAlign = "center"
+}
+function mouseOut() {
+    document.getElementById('screen-img').style.display = 'none'
+    document.getElementById("screen-info").style.display = "none"
+}
+function displayDBI() {
+    let screen = document.getElementById("screen-img");
+       screen.style.backgroundImage = "url('../img/DBI.jpg')"
+       screen.style.display = "block"
+}
+function displayDBIInfo(){
+    let screen = document.getElementById("screen-info")
+    screen.innerHTML = "This is a document about Database Systems"
+    screen.style.background = "#fac1fa"
+    screen.style.fontSize = "50px"
+    screen.style.display = "block"
+    screen.style.textAlign = "center"
+}
+function displayMAD() {
+    let screen = document.getElementById("screen-img");
+       screen.style.backgroundImage = "url('../img/Math.avif')"
+       screen.style.display = "block"
+}
+function displayMADInfo(){
+    let screen = document.getElementById("screen-info")
+    screen.innerHTML = "This is a document about Mathemastic"
+    screen.style.background = "#fac1fa"
+    screen.style.fontSize = "50px"
+    screen.style.display = "block"
+    screen.style.textAlign = "center"
+}
+function displayJS() {
+    let screen = document.getElementById("screen-img");
+       screen.style.backgroundImage = "url('../img/javascript.jpg')"
+       screen.style.display = "block"
+}
+function displayJSInfo(){
+    let screen = document.getElementById("screen-info")
+    screen.innerHTML = "This is a document about Java Script"
+    screen.style.background = "#fac1fa"
+    screen.style.fontSize = "50px"
+    screen.style.display = "block"
+    screen.style.textAlign = "center"
+}
+function displayCSS() {
+    let screen = document.getElementById("screen-img");
+       screen.style.backgroundImage = "url('../img/css.png')"
+       screen.style.display = "block"
+}
+function displayCSSInfo(){
+    let screen = document.getElementById("screen-info")
+    screen.innerHTML = "This is a document about Cascarding Style Sheets"
+    screen.style.background = "#fac1fa"
+    screen.style.fontSize = "50px"
+    screen.style.display = "block"
+    screen.style.textAlign = "center"
+}
